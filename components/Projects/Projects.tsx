1b7f7a6a06c726d91910099406e75e597c5468fa
homfarnam <homfarnam@gmail.com>
2022-11-22 21:12:19 +0100

    Get all project details from props

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,49 +1,24 @@
 import React from 'react'
-import ImageCard from '../ImageCard/ImageCard'
-import SummerIsland from '../../public/images/summer-island.png'
-import Office from '../../public/images/office.png'
-import LightRoom from '../../public/images/light-room.png'
-import GalleryPaint from '../../public/images/gallery-paint.png'
-import ReadMore from '../../public/images/read-more.svg'
-import Image from 'next/image'
 import Project from '../Project/Project'
+import { ImagesListType } from 'types/types'
 
-interface ProjectsProps {}
+interface ProjectsProps {
+  data: ImagesListType[]
+}
 
-const Projects = ({}: ProjectsProps) => {
+const Projects = ({ data }: ProjectsProps) => {
   return (
     <section className="home__works">
-      <Project
-        image={SummerIsland}
-        alt="SummerIsland"
-        title="BOL.COM"
-        description="A Summer island in the Netherlands"
-        withReadMore
-      />
-
-      <Project
-        image={Office}
-        alt="Office"
-        title="KEMPEN"
-        description="Not some average banking website"
-        withReadMore
-      />
-
-      <Project
-        image={LightRoom}
-        alt="LightRoom"
-        title="PHILIPS"
-        description="Beautiful design meets innovative technology"
-        withReadMore
-      />
-
-      <Project
-        image={GalleryPaint}
-        alt="GalleryPaint"
-        title="GEMEENTEMUSEUM"
-        description="A 100 years of Mondriaan & De Stijl"
-        withReadMore
-      />
+      {data.map((item) => (
+        <Project
+          key={item.id}
+          image={item.image}
+          alt={item.alt}
+          title={item.title}
+          description={item.description}
+          withReadMore
+        />
+      ))}
     </section>
   )
 }
