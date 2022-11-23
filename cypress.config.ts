239d3e3d5a3eb4849279b6bcf7ba2db1e17e07b3
homfarnam <homfarnam@gmail.com>
2022-11-24 00:59:15 +0100

    Update cypress config files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+      return config
     },
-    baseUrl: "http://localhost:4000",
+    baseUrl: 'http://localhost:4000',
   },
 
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      framework: 'next',
+      bundler: 'webpack',
     },
   },
-});
+})
