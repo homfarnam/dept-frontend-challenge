84e392220642d67bb7a0e067ceef9514754de5f0
homfarnam <homfarnam@gmail.com>
2022-11-23 21:11:01 +0100

    Add react query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '../styles/globals.css'
 import '../styles/main.scss'
-import type { AppProps } from 'next/app'
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 3000, refetchOnWindowFocus: false } },
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
