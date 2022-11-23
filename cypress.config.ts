2d290c9323aa58f37a440055f9229ef3355e15f3
homfarnam <homfarnam@gmail.com>
2022-11-24 00:41:32 +0100

    Add cypress config files and tests

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:4000",
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,39 @@
+describe('empty spec', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('Render page correctly', () => {
+    cy.visit('/')
+  })
+
+  it('Navbar element', () => {
+    cy.get('header')
+  })
+
+  it('Click on dots and open menu', () => {
+    cy.get('#dots').click()
+    cy.get('.megaMenu')
+  })
+
+  it('Should be visible Home link after click on dots', () => {
+    cy.get('#dots').click()
+    cy.get('.megaMenu')
+    cy.get('.megaMenu').contains('Home')
+    cy.get('#close').click()
+  })
+
+  it('Get contact form and get name input and write into it', () => {
+    cy.get('.home__contactForm')
+
+    cy.get('.home__contactForm').find('input[name="name"]').type('Test')
+
+    cy.get('.home__contactForm')
+      .find('input[name="name"]')
+      .should('have.value', 'Test')
+  })
+
+  it('Click on arrow up and scroll to top', () => {
+    cy.get('#arrow-up').click()
+    cy.scrollTo('top')
+  })
+})
