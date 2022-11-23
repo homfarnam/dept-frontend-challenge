fe881ae8cce4bab70d72e19a1459c1cea832aed2
homfarnam <homfarnam@gmail.com>
2022-11-23 16:02:31 +0100

    Add navlinks file

---
+++ components/shared/Layout/Navlinks.ts
@@ -0,0 +1,89 @@
+import { NavLinksType } from 'types/types'
+
+const DesktopNavLinks: NavLinksType[] = [
+  {
+    id: 1,
+    title: 'Home',
+    link: '/',
+  },
+  {
+    id: 2,
+    title: 'Work',
+    link: '#',
+  },
+  {
+    id: 3,
+    title: 'Culture',
+    link: '#',
+  },
+  {
+    id: 4,
+    title: 'Services',
+    link: '#',
+  },
+  {
+    id: 5,
+    title: 'Insights',
+    link: '#',
+  },
+  {
+    id: 6,
+    title: 'Careers',
+    link: '#',
+  },
+  {
+    id: 7,
+    title: 'Contact',
+    link: '#',
+  },
+]
+
+const MobileNavLinks: NavLinksType[] = [
+  {
+    id: 1,
+    title: 'Home',
+    link: '/',
+  },
+  {
+    id: 2,
+    title: 'Work',
+    link: '#',
+  },
+  {
+    id: 3,
+    title: 'Culture',
+    link: '#',
+  },
+  {
+    id: 4,
+    title: 'Services',
+    link: '#',
+  },
+  {
+    id: 5,
+    title: 'Partners',
+    link: '#',
+  },
+  {
+    id: 6,
+    title: 'Stories',
+    link: '#',
+  },
+  {
+    id: 7,
+    title: 'Careers',
+    link: '#',
+  },
+  {
+    id: 8,
+    title: 'Events',
+    link: '#',
+  },
+  {
+    id: 9,
+    title: 'Contact',
+    link: '#',
+  },
+]
+
+export { DesktopNavLinks, MobileNavLinks }
