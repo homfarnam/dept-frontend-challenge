4db27100e223710866a5d09ff90b7e2b1a2d9579
homfarnam <homfarnam@gmail.com>
2022-11-23 16:01:10 +0100

    Create custom document file

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, {
+  DocumentContext,
+  Head,
+  DocumentInitialProps,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.cdnfonts.com/css/maison-neue"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
