c41effc34a21a5607f9354b42b1ee100929fc4cc
homfarnam <homfarnam@gmail.com>
2022-11-23 22:53:07 +0100

    Handle form states change

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface ContactFormProps {}
 
 const ContactForm = ({}: ContactFormProps) => {
+  const [formData, setFormData] = useState<{
+    name: string
+    email: string
+    message: string
+  }>({
+    name: '',
+    email: '',
+    message: '',
+  })
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
   return (
     <section className="home__contactForm">
       <div className="home__contactForm--title">
@@ -13,16 +29,33 @@ const ContactForm = ({}: ContactFormProps) => {
           <div className="home__contactForm__formContainer__form__topInput">
             <div className="">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" placeholder="Enter your name" />
+              <input
+                type="text"
+                onChange={handleChange}
+                value={formData.name}
+                name="name"
+                placeholder="Enter your name"
+              />
             </div>
             <div className="">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" placeholder="Enter your email" />
+              <input
+                type="email"
+                onChange={handleChange}
+                value={formData.email}
+                name="email"
+                placeholder="Enter your email"
+              />
             </div>
           </div>
           <div className="home__contactForm__formContainer__form__downInputs">
             <label htmlFor="message">message</label>
-            <textarea name="message" id="message" />
+            <textarea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              id="message"
+            />
           </div>
           <div className="home__contactForm__formContainer__form__button">
             <button type="button">Send</button>
